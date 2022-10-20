7b5d52287776c76e6052efb3be2fa18138dbbb01
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-10-21 01:57:39 +0300

    add loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import loadingImg from "../img/loading.gif";
+const Loading = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <img src={loadingImg} alt="" />
+    </div>
+  );
+};
+
+export default Loading;
